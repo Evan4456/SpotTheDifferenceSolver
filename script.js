30a77d287f76f0d6553c1ed47210930d34433342
Evan4456 <evanlong@bell.net>
2023-11-06 19:17:40 -0500

    now circles the differences according to the cluster's radius

--- script.js
+++ script.js
@@ -33,124 +33,6 @@ function setPixel(imgData, index, r, g, b, a) {
     d[i + 3] = a;
 }
 
-/*function spotTheDiff() {
-    var canvasOne = document.getElementById("ImgDisOne");
-    var canvasTwo = document.getElementById("ImgDisTwo");
-
-    var diffs = document.getElementById("diffs");
-
-    diffs.width = canvasOne.width;
-    diffs.height = canvasOne.height;
-
-    var contextOne = canvasOne.getContext("2d");
-    var contextTwo = canvasTwo.getContext("2d");
-
-    var diffsContext = diffs.getContext("2d");
-    var diffsImage = diffsContext.getImageData(0, 0, diffs.width, diffs.height);
-
-    if (canvasOne.width != canvasTwo.width || canvasOne.height != canvasTwo.height) {
-        alert("Images must be the same size");
-        return;
-    }
-
-    var imageDataOne = contextOne.getImageData(0, 0, canvasOne.width, canvasOne.height);
-    var imageDataTwo = contextTwo.getImageData(0, 0, canvasTwo.width, canvasTwo.height);
-
-    for (let i = 0; i < canvasOne.width * canvasOne.height; i++) {
-        var pxOne = getPixel(imageDataOne, i);
-        var pxTwo = getPixel(imageDataTwo, i);
-        if (JSON.stringify(pxOne) === JSON.stringify(pxTwo)) {
-
-        } else {
-            //setPixel(diffsImage, i , 57, 255, 20, 150);
-            setPixel(imageDataOne, i, 57, 255, 20, 150);
-            setPixel(imageDataTwo, i, 57, 255, 20, 150);
-            //console.log("pixel being set");
-        }
-    }
-    //diffsContext.putImageData(diffsImage, 0, 0);
-    contextOne.putImageData(imageDataOne, 0, 0);
-    contextTwo.putImageData(imageDataTwo, 0, 0);
-
-    return;
-}*/
-
-function spotTheDiffClustersKMeans() {
-    var canvasOne = document.getElementById("ImgDisOne");
-    var canvasTwo = document.getElementById("ImgDisTwo");
-
-    var contextOne = canvasOne.getContext("2d");
-    var contextTwo = canvasTwo.getContext("2d");
-
-    if (canvasOne.width != canvasTwo.width || canvasOne.height != canvasTwo.height) {
-        alert("Images must be the same size");
-        return;
-    }
-
-    var imageDataOne = contextOne.getImageData(0, 0, canvasOne.width, canvasOne.height);
-    var imageDataTwo = contextTwo.getImageData(0, 0, canvasTwo.width, canvasTwo.height);
-
-    var vectors = new Array;
-    var pixelCount = 0;
-
-    for (let w = 0; w < canvasOne.width; w++) {
-        for (let h = 0; h < canvasOne.height; h++) {
-            if (JSON.stringify(getPixel(imageDataOne, h * canvasOne.width + w)) === JSON.stringify(getPixel(imageDataTwo, h * canvasOne.width + w))) {
-
-            } else {
-                vectors[pixelCount] = [w, h];
-                pixelCount = pixelCount + 1;
-            }
-        }
-    }
-
-    console.log(vectors);
-
-    var clusters = figue.kmeans(document.getElementById("numDiffs").value, vectors);
-
-    for (let i = 0; i < vectors.length; i++) {
-        var cluster = clusters.assignments[i];
-
-        switch (cluster) {
-            case 0:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 14, 185, 12, 255); // green rgba(14, 185, 12, 1)
-                break;
-            case 1:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 255, 0, 0, 255); // red
-                break;
-            case 2:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 0, 255, 0, 255); //yellow
-                break;
-            case 3:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 0, 0, 255, 255); //blue
-                break;
-            case 4:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 255, 0, 205, 255); //pink
-                break;
-            case 5:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 0, 255, 255, 255); //"tuquois"
-                break;
-            case 6:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 255, 162, 0, 255); //orange
-                break;
-            case 7:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 0, 0, 0, 255); //black
-                break;
-            case 8:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 255, 255, 255, 255); //white
-                break;
-            case 9:
-                setPixel(imageDataOne, vectors[i][1] * canvasOne.width + vectors[i][0], 150, 150, 150, 255); //grey
-                break;
-        }
-
-    }
-
-    contextOne.putImageData(imageDataOne, 0, 0);
-    contextTwo.putImageData(imageDataTwo, 0, 0);
-
-    return;
-}
 
 function spotTheDiffClustersDBSCAN(){
     var canvasOne = document.getElementById("ImgDisOne");
@@ -170,6 +52,7 @@ function spotTheDiffClustersDBSCAN(){
     let data = [];
     var dataIndex = 0;
 
+    //find non matching pixels
     for (let w = 0; w < canvasOne.width; w++) {
         for (let h = 0; h < canvasOne.height; h++) {
             if (JSON.stringify(getPixel(imageDataOne, h * canvasOne.width + w)) === JSON.stringify(getPixel(imageDataTwo, h * canvasOne.width + w))) {
@@ -185,49 +68,108 @@ function spotTheDiffClustersDBSCAN(){
         }
     }
 
-
+    //run density based scan 
     var dbScanner = jDBSCAN().eps(12).minPts(1).distance('EUCLIDEAN').data(data);
 
     var point_assignment_result = dbScanner();
 
-    
+    //count the differences 
+    var numDiffs = 0;
+
+    for(var i = 0; i < point_assignment_result.length; i++){
+
+        if (point_assignment_result[i] > numDiffs){
+            numDiffs = point_assignment_result[i];
+        }
+
+    }
+
+    var sum_x = 0;
+    var sum_y = 0;
+    var num_points = 0;
+
+    // draw circles 
+    for(var i = 0; i < numDiffs; i++){
+        //calculate the center of the cluster of different pixels
+        for(var j = 0; j < point_assignment_result.length; j++){
+            if(point_assignment_result[j] == i){
+                sum_x += data[j].x;
+                sum_y += data[j].y;
+                num_points = num_points + 1;
+            }
+        }
+
+        var center_x = sum_x / num_points;
+        var center_y = sum_y / num_points;
+
+        //get radius of circle
+        var max_distance_from_center = 0;
+
+        for(var j = 0; j < point_assignment_result.length; j++){
+            if(point_assignment_result[j] == i){
+                var dist = Math.sqrt(((data[j].x - center_x) * (data[j].x - center_x)) + ((data[j].y - center_y) * (data[j].y - center_y)));
+
+                if (Math.abs(dist) > max_distance_from_center){
+                    max_distance_from_center = Math.abs(dist);
+                }
+            }
+        }
+
+        //use that center and radius to draw a circle
+        contextOne.beginPath();
+        contextOne.arc(center_x, center_y, max_distance_from_center ,0 , 2 * 3.14159); // 10 ought to become a radius
+        contextOne.strokeStyle = "blue";
+        contextOne.stroke();
+
+        contextTwo.beginPath();
+        contextTwo.arc(center_x, center_y, max_distance_from_center ,0 , 2 * 3.14159); // 10 ought to become a radius
+        contextTwo.strokeStyle = "blue";
+        contextTwo.stroke();
+        sum_x = 0;
+        sum_y = 0;
+        num_points = 0;
+        max_distance_from_center = 0;
+    }
+
+    /*
     for(var i = 0; i < point_assignment_result.length; i++){
 
         switch (point_assignment_result[i]) {
             case 10:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 14, 185, 12, 255); // green rgba(14, 185, 12, 1)
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 14, 185, 12, 111); // green rgba(14, 185, 12, 1)
                 break;
             case 1:
-                setPixel(imageDataOne,data[i].y * canvasOne.width + data[i].x, 255, 0, 0, 255); // red
+                setPixel(imageDataOne,data[i].y * canvasOne.width + data[i].x, 255, 0, 0, 111); // red
                 break;
             case 2:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 255, 0, 255); //yellow
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 255, 0, 111); //yellow
                 break;
             case 3:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 0, 255, 255); //blue
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 0, 255, 111); //blue
                 break;
             case 4:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 255, 0, 205, 255); //pink
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 255, 0, 205, 111); //pink
                 break;
             case 5:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 255, 255, 255); //"tuquois"
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 255, 255, 111); //"tuquois"
                 break;
             case 6:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 255, 162, 0, 255); //orange
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 255, 162, 0, 111); //orange
                 break;
             case 7:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 0, 0, 255); //black
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 0, 0, 0, 111); //black
                 break;
             case 8:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 255, 255, 255, 255); //white
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 255, 255, 255, 111); //white
                 break;
             case 9:
-                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 150, 150, 150, 255); //grey
+                setPixel(imageDataOne, data[i].y * canvasOne.width + data[i].x, 150, 150, 150, 111); //grey
                 break;
         }
+
     }
 
     contextOne.putImageData(imageDataOne, 0, 0);
-    contextTwo.putImageData(imageDataTwo, 0, 0);
+    //contextTwo.putImageData(imageDataTwo, 0, 0);*/
     
 }
